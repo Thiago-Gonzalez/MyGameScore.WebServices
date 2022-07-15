53c5e25bbeac853ce0d3cfde50eb1f04ca930af5
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-15 17:24:39 -0300

    feature/implement-match-and-player-repositories-infrastructure-layer

---
+++ MyGameScore.Infrastructure/Persistence/Repositories/MatchRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyGameScore.Core.Entities;
+using MyGameScore.Core.Repositories;
+
+namespace MyGameScore.Infrastructure.Persistence.Repositories
+{
+    public class MatchRepository : IMatchRepository
+    {
+        private readonly MyGameScoreDbContext _dbContext;
+        public MatchRepository(MyGameScoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Match match)
+        {
+            await _dbContext.Matches.AddAsync(match);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Match>> GetAllAsync()
+        {
+            return await _dbContext.Matches.ToListAsync();
+        }
+
+        public async Task<List<Match>> GetPlayerMatchesAsync(int idPlayer)
+        {
+            var matches = await _dbContext.Matches.Where(m => m.IdPlayer == idPlayer).ToListAsync();
+
+            if (matches == null) return null;
+
+            return matches;
+        }
+
+        public async Task<Match> GetByIdAsync(int id)
+        {
+            var match = await _dbContext.Matches
+                .Include(m => m.Player)
+                .SingleOrDefaultAsync(m => m.Id == id);
+
+            if (match == null) return null;
+
+            return match;    
+        }
+    }
+}
---
+++ MyGameScore.Infrastructure/Persistence/Repositories/PlayerRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyGameScore.Core.Entities;
+using MyGameScore.Core.Repositories;
+
+namespace MyGameScore.Infrastructure.Persistence.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly MyGameScoreDbContext _dbContext;
+        public PlayerRepository(MyGameScoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CreatePlayerAsync(Player player)
+        {
+            await _dbContext.Players.AddAsync(player);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Player> GetByIdAsync(int id)
+        {
+            return await _dbContext.Players.SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Player> GetPlayerByEmailAndPasswordAsync(string email, string passwordHash)
+        {
+            return await _dbContext
+                .Players
+                .SingleOrDefaultAsync(p => p.Email == email && p.Password == passwordHash);
+        }
+    }
+}
