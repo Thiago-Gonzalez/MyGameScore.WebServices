7a5839855a3e35855dea2894186d7207dd688a32
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-15 16:52:30 -0300

    feature/implement-dbcontext-configurations

---
+++ MyGameScore.Infrastructure/Persistence/Configurations/MatchConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyGameScore.Core.Entities;
+
+namespace MyGameScore.Infrastructure.Persistence.Configurations
+{
+    public class MatchConfigurations : IEntityTypeConfiguration<Match>
+    {
+        public void Configure(EntityTypeBuilder<Match> builder)
+        {
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .HasOne(m => m.Player)
+                .WithMany(m => m.Matches)
+                .HasForeignKey(m => m.IdPlayer)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ MyGameScore.Infrastructure/Persistence/Configurations/PlayerConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyGameScore.Core.Entities;
+
+namespace MyGameScore.Infrastructure.Persistence.Configurations
+{
+    public class PlayerConfigurations : IEntityTypeConfiguration<Player>
+    {
+        public void Configure(EntityTypeBuilder<Player> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+        }
+    }
+}
