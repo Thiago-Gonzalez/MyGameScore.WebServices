079ceb5bbc5e3d6a2ace6022ee9c628c8fbdc5bd
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-15 17:15:57 -0300

    feature/implement-player-and-match-repository-interfaces

---
+++ MyGameScore.Core/Repositories/IMatchRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyGameScore.Core.Entities;
+
+namespace MyGameScore.Core.Repositories
+{
+    public interface IMatchRepository
+    {
+        Task<List<Match>> GetAllAsync();
+        Task<List<Match>> GetPlayerMatchesAsync(int idPlayer);
+        Task<Match> GetByIdAsync(int id);
+        Task AddAsync(Match match);
+    }
+}
---
+++ MyGameScore.Core/Repositories/IPlayerRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyGameScore.Core.Entities;
+
+namespace MyGameScore.Core.Repositories
+{
+    public interface IPlayerRepository
+    {
+        Task<Player> GetByIdAsync(int id);
+        Task CreatePlayerAsync(Player player);
+        Task<Player> GetPlayerByEmailAndPasswordAsync(string email, string passwordHash);
+    }
+}
