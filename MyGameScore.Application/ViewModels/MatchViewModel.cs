4baff84ee4c3ba3cd399385229fb4696141bbb22
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-16 14:22:43 -0300

    feature/implement-view-models

---
+++ MyGameScore.Application/ViewModels/MatchViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MyGameScore.Application.ViewModels
+{
+    public class MatchViewModel
+    {
+        public MatchViewModel(int id, DateTime date, int score, string playerName)
+        {
+            Id = id;
+            Date = date;
+            Score = score;
+            PlayerName = playerName;
+        }
+
+        public int Id { get; private set; }
+        public DateTime Date { get; private set; }
+        public int Score { get; private set; }
+        public string PlayerName { get; private set; }
+    }
+}
