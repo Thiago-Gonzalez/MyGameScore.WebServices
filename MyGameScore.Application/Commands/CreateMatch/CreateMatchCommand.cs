e8a34fb63ba6b33ee8922f4f3b425bce6e49d1e3
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-16 16:04:28 -0300

    feature/implement-create-match-command

---
+++ MyGameScore.Application/Commands/CreateMatch/CreateMatchCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MyGameScore.Application.Commands.CreateMatch
+{
+    public class CreateMatchCommand : IRequest<int>
+    {
+        public DateTime Date { get; private set; }
+        public int Score { get; private set; }
+        public int IdPlayer { get; private set; }
+    }
+}
