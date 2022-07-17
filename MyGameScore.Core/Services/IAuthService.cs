59deabe26e2f54f4bc38c53b99c7cedb31bcc715
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-17 14:58:58 -0300

    feature/implement-auth-service-interface-and-configurations

---
+++ MyGameScore.Core/Services/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿namespace MyGameScore.Core.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string email);
+        string ComputeSha256Hash(string password);
+    }
+}
