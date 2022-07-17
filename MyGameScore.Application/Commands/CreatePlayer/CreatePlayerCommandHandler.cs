1158dbcb2807deb1ce8aadfae9381fc135957d35
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-17 15:45:05 -0300

    feature/implement-CreatePlayer-and-LoginPlayer-Commands

---
+++ MyGameScore.Application/Commands/CreatePlayer/CreatePlayerCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MyGameScore.Core.Entities;
+using MyGameScore.Core.Repositories;
+using MyGameScore.Core.Services;
+
+namespace MyGameScore.Application.Commands.CreatePlayer
+{
+    public class CreatePlayerCommandHandler : IRequestHandler<CreatePlayerCommand, int>
+    {
+        private readonly IPlayerRepository _playerRepository;
+        private readonly IAuthService _authService;
+        public CreatePlayerCommandHandler(IPlayerRepository playerRepository, IAuthService authService)
+        {
+            _playerRepository = playerRepository;
+            _authService = authService;
+        }
+
+        public async Task<int> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+        {
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            var player = new Player(request.Name, request.Email, passwordHash);
+
+            await _playerRepository.CreatePlayerAsync(player);
+
+            return player.Id;
+        }
+    }
+}
