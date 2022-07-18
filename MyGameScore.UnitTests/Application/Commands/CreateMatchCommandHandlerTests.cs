ca63132dded06d2565b5f022f3b56dbd090e770e
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-18 00:15:01 -0300

    test/implement-creatematch-and-createplayer-commandhandler-tests

---
+++ MyGameScore.UnitTests/Application/Commands/CreateMatchCommandHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using MyGameScore.Application.Commands.CreateMatch;
+using MyGameScore.Core.Repositories;
+using MyGameScore.Core.Entities;
+
+namespace MyGameScore.UnitTests.Application.Commands
+{
+    public class CreateMatchCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnMatchId()
+        {
+            // Arrange
+            var matchRepositoryMock = new Mock<IMatchRepository>();
+
+            var createMatchCommand = new CreateMatchCommand
+            {
+                Date = DateTime.Now,
+                Score = 22,
+                IdPlayer = 1
+            };
+
+            var createMatchCommandHandler = new CreateMatchCommandHandler(matchRepositoryMock.Object);
+
+            // Act
+            var id = await createMatchCommandHandler.Handle(createMatchCommand, new CancellationToken());
+
+            // Assert
+            Assert.True(id >= 0);
+
+            matchRepositoryMock.Verify(mr => mr.AddAsync(It.IsAny<MyGameScore.Core.Entities.Match>()), Times.Once);
+        }
+    }
+}
